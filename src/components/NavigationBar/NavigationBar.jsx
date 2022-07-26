a2270d851ccb137aa805b0c7561f752c388404f6
Tsekai Lee <3172251541@qq.com>
2022-07-26 22:43:27 +0800

    导航栏制作以及路由修改

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,20 +1,55 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import styles from './NavigationBar.module.scss'
-import Bar from './bar'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import style from "./NavigationBar.module.scss";
+import Bar from "./bar";
 
 const NavigationBar = () => {
+  const [barlist, setBarList] = useState([
+    {
+      content: "Gitee",
+      active: false,
+    },
+    {
+      content: "Github",
+      active: false,
+    },
+  ]);
+  const changeStateBar = (clickIndex) => {
+    const newBarlist = barlist.map((item, index) => {
+      if (clickIndex === index) {
+        return {
+          ...item,
+          active: !item.active,
+        };
+      } else {
+        return {
+          ...item,
+          active: false,
+        };
+      }
+    });
+    setBarList(newBarlist);
+  };
+
   return (
-    <header className={styles.container}>
-      <Link to='/'>
-        <h3 className={styles.logo}>8xbitsDevs</h3>
-      </Link >
-      <div className={styles.barLayout}>
-        <Bar name='Gitee'></Bar>
-        <Bar name='Github'></Bar>
+    <header className={style.container}>
+      <Link to="/">
+        <h3 className={style.logo}>8xbitsDevs</h3>
+      </Link>
+      <div className={style.barLayout}>
+        {barlist.map((item, index) => {
+          return (
+            <Bar
+              key={index}
+              content={item.content}
+              active={item.active}
+              click={() => changeStateBar(index)}
+            ></Bar>
+          );
+        })}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
--- src/components/NavigationBar/bar/index.jsx
+++ src/components/NavigationBar/bar/index.jsx
@@ -1,18 +1,13 @@
 import React, { useRef } from 'react'
 import { useEffect, useState } from 'react'
-import styles from './index.module.scss'
+import style from './index.module.scss'
+import PubSub from 'pubsub-js'
+
 function Bar(props) {
-    const bar = useRef()
-    useEffect(() => {
-        console.log(props.name)
-        console.log(bar.current.innerHTML)
-        bar.current.innerHTML = props.name
-    },[])
+    const { click, content, active } = props;
 
     return (
-        <div ref={bar} className={styles.bar}>
-        
-        </div>
+        <div className={!active ? style.bar : `${style.bar} ${style.bar_active}`} onClick={() => click()} >{content}</div>
     )
 }
 
